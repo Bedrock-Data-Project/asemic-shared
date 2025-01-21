4a65dc648c17016fe10cf51953f9a013c17b3481
ikorhner <ikorhner@godaddy.com>
2025-01-21 22:00:30 +0100

    add support for returning real step name

--- sequence/src/main/java/com/asemicanalytics/sequence/sequence/Step.java
+++ sequence/src/main/java/com/asemicanalytics/sequence/sequence/Step.java
@@ -1,9 +1,22 @@
 package com.asemicanalytics.sequence.sequence;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public interface Step {
   List<String> getStepNames();
+
+  default List<String> getRealStepNames(Collection<DomainStep> domainSteps) {
+    return getStepNames().stream().map(stepName -> {
+      for (DomainStep domainStep : domainSteps) {
+        if (domainStep.alias().orElse("").equals(stepName)) {
+          return domainStep.actionLogicalTableName();
+        }
+      }
+      return stepName;
+    }).collect(Collectors.toList());
+  }
 
   int getIndex();
 
